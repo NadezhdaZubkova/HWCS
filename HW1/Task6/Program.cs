34ef17edc935cb9df43d661440c2cb1ceb0c2a7e
Nadezhda Zubkova <bedlamite26@gmail.com>
2022-09-06 13:15:06 +0300

    Задача6

---
+++ HW1/Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+int a;
+Console.WriteLine ("Введите число: ") ;
+a =Convert.ToInt32(Console.ReadLine()) ;
+if ( a%2== 0 ) 
+{
+    Console.Write ("Четное") ;
+}
+else
+{
+    Console.Write ("НЕчетное") ;
+}
